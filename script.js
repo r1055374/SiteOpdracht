33cfe0b477133ce7073d906f6c29a34eedd4b2c7
r1055374 <r1055374@ucll.be>
2024-12-17 13:46:06 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.querySelector("h1").innerText = "Marvel Movies";
+
+document.getElementById("geenfilms").remove();
+
+function addMovie(titel, korteDesc, score, director) {
+  const filmArtikel = document.createElement("article");
+  filmArtikel.innerHTML = `
+    <p class="score">${score}</p>
+    <h2 class="movieTitle">${titel}</h2>
+    <p>${korteDesc}</p>
+    <p class="director">${director}</p>
+  `;
+  document.querySelector("section.films").appendChild(filmArtikel);
+}
+
+addMovie(
+  "Iron Man",
+  "A man gets gravely wounded and decides to escape and from that moment onward decides to do no harm and makes a suit of armor to protect others.",
+  "85",
+  "Jon Favreau"
+);
